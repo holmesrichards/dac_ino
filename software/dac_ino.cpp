87681ec492a6768989c712d85999848e168e3485
Rich Holmes <rs.holmes@gmail.com>
2020-05-21 22:11:44 -0400

    Separate in and out nomenclature. Add option to configure two gate pins as input or output.

--- software/dac_ino.cpp
+++ software/dac_ino.cpp
@@ -21,17 +21,17 @@ and removal of code to control offset
 
 namespace
 {
-static constexpr uint8_t k_writeChannelA = 0b01110000;
-static constexpr uint8_t k_writeChannelB = 0b11110000;
-
-static constexpr uint8_t k_pinCVInA = A0;
-static constexpr uint8_t k_pinCVInB = A1;
-static constexpr uint8_t k_pinCVInC = A2;
-static constexpr uint8_t k_pinCVInD = A3;
-static constexpr uint8_t k_pinCVInE = A4;
-static constexpr uint8_t k_pinCVInF = A5;
-static constexpr uint8_t k_pinCVInG = A6;
-static constexpr uint8_t k_pinCVInH = A7;
+  static constexpr uint8_t k_writeChannelA = 0b01110000;
+  static constexpr uint8_t k_writeChannelB = 0b11110000;
+  
+  static constexpr uint8_t k_pinCVInA = A0;
+  static constexpr uint8_t k_pinCVInB = A1;
+  static constexpr uint8_t k_pinCVInC = A2;
+  static constexpr uint8_t k_pinCVInD = A3;
+  static constexpr uint8_t k_pinCVInE = A4;
+  static constexpr uint8_t k_pinCVInF = A5;
+  static constexpr uint8_t k_pinCVInG = A6;
+  static constexpr uint8_t k_pinCVInH = A7;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -55,11 +55,16 @@ void dac_ino::begin(unsigned spiDivider_)
   pinMode(k_pinGateInD, INPUT);
   pinMode(k_pinGateInE, INPUT);
   pinMode(k_pinGateInF, INPUT);
-
+  
   pinMode(k_pinChipSelectDAC, OUTPUT);
   pinMode(k_pinGateOutA, OUTPUT);
   pinMode(k_pinGateOutB, OUTPUT);
 
+  m_gateInEEnabled = true;
+  m_gateInFEnabled = true;
+  m_gateOutCEnabled = false;
+  m_gateOutDEnabled = false;
+  
 #if defined __AVR__
   m_outputChipSelectDAC = HIGH;
 #else
@@ -75,25 +80,67 @@ void dac_ino::begin(unsigned spiDivider_)
 
 //--------------------------------------------------------------------------------------------------
 
-unsigned dac_ino::readCV(CVChannel channel_)
+void dac_ino::setGateToOutput (GateOutChannel channel_);
+{
+  switch (channel_)
+    {
+    case GateOutChannel::C:
+      pinMode(k_pinGateOutC, OUTPUT);
+      m_gateOutCEnabled = true;
+      m_gateInEEnabled = false;
+      break;
+    case GateOutChannel::D:
+      pinMode(k_pinGateOutD, OUTPUT);
+      m_gateOutDEnabled = true;
+      m_gateInFEnabled = false;
+      break;
+    default:
+      return;
+    }
+}
+  
+//--------------------------------------------------------------------------------------------------
+
+void dac_ino::setGateToInput (GateInChannel channel_);
+{
+  switch (channel_)
+    {
+    case GateInChannel::E:
+      pinMode(k_pinGateInE, INPUT);
+      m_gateInEEnabled = true;
+      m_gateOutCEnabled = false;
+      break;
+    case GateInChannel::F:
+      pinMode(k_pinGateInF, OUTPUT);
+      m_gateInFEnabled = true;
+      m_gateOutDEnabled = false;
+      break;
+    default:
+      return;
+    }
+}
+  
+//--------------------------------------------------------------------------------------------------
+
+unsigned dac_ino::readCV(CVInChannel channel_)
 {
   switch (channel_)
   {
-    case CVChannel::A:
+    case CVInChannel::A:
       return analogRead(k_pinCVInA);
-    case CVChannel::B:
+    case CVInChannel::B:
       return analogRead(k_pinCVInB);
-    case CVChannel::C:
+    case CVInChannel::C:
       return analogRead(k_pinCVInC);
-    case CVChannel::D:
+    case CVInChannel::D:
       return analogRead(k_pinCVInD);
-    case CVChannel::E:
+    case CVInChannel::E:
       return analogRead(k_pinCVInE);
-    case CVChannel::F:
+    case CVInChannel::F:
       return analogRead(k_pinCVInF);
-    case CVChannel::G:
+    case CVInChannel::G:
       return analogRead(k_pinCVInG);
-    case CVChannel::H:
+    case CVInChannel::H:
       return analogRead(k_pinCVInH);
     default:
       return 0U;
@@ -102,21 +149,21 @@ unsigned dac_ino::readCV(CVChannel channel_)
 
 //--------------------------------------------------------------------------------------------------
 
-void dac_ino::writeCV(CVChannel channel_, uint16_t value_)
+void dac_ino::writeCV(CVOutChannel channel_, uint16_t value_)
 {
   uint8_t msg1 = (uint8_t)((value_ >> 8) & 0x0F);
   uint8_t msg2 = (uint8_t)(value_ & 0xFF);
 
   switch (channel_)
   {
-    case CVChannel::A:
+    case CVOutChannel::A:
     {
-      msg1 |= k_writeChannelA;
+      msg1 |= k_writeOutChannelA;
       break;
     }
-    case CVChannel::B:
+    case CVOutChannel::B:
     {
-      msg1 |= k_writeChannelB;
+      msg1 |= k_writeOutChannelB;
       break;
     }
     default:
@@ -143,52 +190,59 @@ void dac_ino::writeCV(CVChannel channel_, uint16_t value_)
 
 //--------------------------------------------------------------------------------------------------
 
-bool dac_ino::readGate(GateChannel channel_)
+bool dac_ino::readGate(GateInChannel channel_)
 {
+  // Channels A through D have inverting input circuitry!
+  // Channels E and F do not.
+  
   switch (channel_)
   {
-    case GateChannel::A:
+    case GateInChannel::A:
     {
 #if defined __AVR__
-      return m_inputGateA;
+      return !(m_inputGateA);
 #else
-      return digitalRead(k_pinGateInA);
+      return !(digitalRead(k_pinGateInA));
 #endif
     }
-    case GateChannel::B:
+    case GateInChannel::B:
     {
 #if defined __AVR__
-      return m_inputGateB;
+      return !(m_inputGateB);
 #else
-      return digitalRead(k_pinGateInB);
+      return !(digitalRead(k_pinGateInB));
 #endif
     }
-    case GateChannel::C:
+    case GateInChannel::C:
     {
 #if defined __AVR__
-      return m_inputGateC;
+      return !(m_inputGateC);
 #else
-      return digitalRead(k_pinGateInC);
+      return !(digitalRead(k_pinGateInC));
 #endif
     }
-    case GateChannel::D:
+    case GateInChannel::D:
     {
 #if defined __AVR__
-      return m_inputGateD;
+      return !(m_inputGateD);
 #else
-      return digitalRead(k_pinGateInD);
+      return !(digitalRead(k_pinGateInD));
 #endif
     }
-    case GateChannel::E:
+    case GateInChannel::E:
     {
+      if (!m_gateInEEnabled)
+	return false;
 #if defined __AVR__
       return m_inputGateE;
 #else
       return digitalRead(k_pinGateInE);
 #endif
     }
-    case GateChannel::F:
+    case GateInChannel::F:
     {
+      if (!m_gateInFEnabled)
+	return false;
 #if defined __AVR__
       return m_inputGateF;
 #else
@@ -204,11 +258,11 @@ bool dac_ino::readGate(GateChannel channel_)
 
 //--------------------------------------------------------------------------------------------------
 
-void dac_ino::writeGate(GateChannel channel_, bool state_)
+void dac_ino::writeGate(GateOutChannel channel_, bool state_)
 {
   switch (channel_)
   {
-    case GateChannel::A:
+    case GateOutChannel::A:
     {
 #if defined __AVR__
       m_outputGateA = state_ ? LOW : HIGH;
@@ -218,12 +272,35 @@ void dac_ino::writeGate(GateChannel channel_, bool state_)
 
       break;
     }
-    case GateChannel::B:
+    case GateOutChannel::B:
     {
 #if defined __AVR__
       m_outputGateB = state_ ? LOW : HIGH;
 #else
       digitalWrite(k_pinGateOutB, (state_ ? LOW : HIGH));
+#endif
+      break;
+    }
+    case GateOutChannel::C:
+    {
+      if (!m_gateOutCEnabled)
+	break;
+#if defined __AVR__
+      m_outputGateC = state_ ? LOW : HIGH;
+#else
+      digitalWrite(k_pinGateOutC, (state_ ? LOW : HIGH));
+#endif
+
+      break;
+    }
+    case GateOutChannel::D:
+    {
+      if (!m_gateOutDEnabled)
+	break;
+#if defined __AVR__
+      m_outputGateD = state_ ? LOW : HIGH;
+#else
+      digitalWrite(k_pinGateOutD, (state_ ? LOW : HIGH));
 #endif
       break;
     }
@@ -236,36 +313,36 @@ void dac_ino::writeGate(GateChannel channel_, bool state_)
 
 //--------------------------------------------------------------------------------------------------
 
-void dac_ino::gateInputInterrupt(GateChannel channel_, void (*callback_)(void), GateInterrupt mode_)
+void dac_ino::gateInputInterrupt(GateInChannel channel_, void (*callback_)(void), GateInterrupt mode_)
 {
   switch (channel_)
   {
-    case GateChannel::A:
+    case GateInChannel::A:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInA), callback_, static_cast<uint32_t>(mode_));
       break;
     }
-    case GateChannel::B:
+    case GateInChannel::B:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInB), callback_, static_cast<uint32_t>(mode_));
       break;
     }
-    case GateChannel::C:
+    case GateInChannel::C:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInC), callback_, static_cast<uint32_t>(mode_));
       break;
     }
-    case GateChannel::D:
+    case GateInChannel::D:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInD), callback_, static_cast<uint32_t>(mode_));
       break;
     }
-    case GateChannel::E:
+    case GateInChannel::E:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInE), callback_, static_cast<uint32_t>(mode_));
       break;
     }
-    case GateChannel::F:
+    case GateInChannel::F:
     {
       attachInterrupt(digitalPinToInterrupt(k_pinGateInF), callback_, static_cast<uint32_t>(mode_));
       break;
--- software/dac_ino.h
+++ software/dac_ino.h
@@ -31,11 +31,36 @@ namespace dcrd
   \class dac_inoBoard
   \brief This class is used to configure and handle the dac/ino board for Arduino
 */
+
+/*
+The dac/ino board has:
+- 4 gate inputs
+- 2 gate outputs
+- 4 CV inputs
+- 2 CV outputs
+- 2 additional digital pins
+- 4 additional analog pins for input
+User can specify whether to configure the additional digital pins for input
+or output.
+
+dac/ino lite board has:
+- 0 gate inputs
+- 1 gate output
+- 0 CV inputs
+- 2 CV outputs
+- 4 additional digital pins
+- 4 additional analog pins for input
+
+Herein we use "CV" and "Gate" to refer to analog and digital input/output
+even though they may not be actual CVs or gates.
+
+*/
+  
 class dac_ino
 {
 public:
 
-  enum class CVChannel : uint8_t
+  enum class CVInChannel : uint8_t
   {
     A, //!< CV channel A
     B, //!< CV channel B
@@ -48,7 +73,14 @@ public:
     Unknown, //!< Unknown/unspecified
   };
 
-  enum class GateChannel : uint8_t
+  enum class CVOutChannel : uint8_t
+  {
+    A, //!< CV channel A
+    B, //!< CV channel B
+    Unknown, //!< Unknown/unspecified
+  };
+
+  enum class GateInChannel : uint8_t
   {
     A, //!< Gate channel A
     B, //!< Gate channel B
@@ -56,6 +88,15 @@ public:
     D, //!< Gate channel D
     E, //!< Gate channel E
     F, //!< Gate channel F
+    Unknown, //!< Unknown/unspecified
+  };
+
+  enum class GateOutChannel : uint8_t
+  {
+    A, //!< Gate channel A
+    B, //!< Gate channel B
+    C, //!< Gate channel C
+    D, //!< Gate channel D
     Unknown, //!< Unknown/unspecified
   };
 
@@ -76,32 +117,44 @@ public:
   */
   void begin(unsigned spiDivider_ = SPI_CLOCK_DIV2);
 
+  //! Set gate channel to output
+  /*!
+    \param channel_  The selected output channel (C or D)
+  */
+  void setGateToOutput (GateOutChannel channel_);
+
+    //! Set gate channel to input
+  /*!
+    \param channel_  The selected input channel (E or F)
+  */
+  void setGateToInput (GateInChannel channel_);
+
   //! Read from a CV input channel
   /*!
-    \param channel_  The channel to read from
+    \param channel_  The input channel to read from
     \return          The value measured on the selected channel
   */
-  unsigned readCV(CVChannel channel_);
+  unsigned readCV(CVInChannel channel_);
 
   //! Write to a CV output channel
   /*!
-    \param channel_  The channel to write to
+    \param channel_  The output channel to write to
   */
-  void writeCV(CVChannel channel_, uint16_t value_);
+  void writeCV(CVOutChannel channel_, uint16_t value_);
 
   //! Read from a gate input channel
   /*!
-    \param channel_  The selected channel
+    \param channel_  The selected input channel
     \return          True if the selected gate input channel is HIGH, false otherwise
   */
-  bool readGate(GateChannel channel_);
+  bool readGate(GateInChannel channel_);
 
   //! Write to a gate output channel
   /*!
-    \param channel_  The selected channel
+    \param channel_  The selected output channel
     \param state_    The logical state (true = HIGH, false = LOW)
   */
-  void writeGate(GateChannel channel_, bool state_);
+  void writeGate(GateOutChannel channel_, bool state_);
 
   //! Attach an interrupt to a gate input channel
   /*!
@@ -109,7 +162,7 @@ public:
     \param callback_  The callback (void function)
     \param mode_      The interrupt condition
   */
-  void gateInputInterrupt(GateChannel channel_,
+  void gateInputInterrupt(GateInChannel channel_,
     void (*callback_)(void),
     GateInterrupt mode_ = GateInterrupt::ValueChange);
 
@@ -126,9 +179,16 @@ private:
   static constexpr uint8_t  k_pinGateInF = 9;
   static constexpr uint8_t  k_pinGateOutA = 6;
   static constexpr uint8_t  k_pinGateOutB = 7;
+  static constexpr uint8_t  k_pinGateOutC = 8;
+  static constexpr uint8_t  k_pinGateOutD = 9;
 
+  static bool m_gateInEEnabled = false;
+  static bool m_gateInFEnabled = false;
+  static bool m_gateOutCEnabled = false;
+  static bool m_gateOutDEnabled = false;
+    
   void setSPIDivider(unsigned spiDivider_);
-
+  
 #if defined __AVR__
   Input<k_pinGateInA> m_inputGateA;
   Input<k_pinGateInB> m_inputGateB;
@@ -140,6 +200,8 @@ private:
   Output<k_pinChipSelectDAC>  m_outputChipSelectDAC;
   Output<k_pinGateOutA>       m_outputGateA;
   Output<k_pinGateOutB>       m_outputGateB;
+  Output<k_pinGateOutC>       m_outputGateC;
+  Output<k_pinGateOutD>       m_outputGateD;
 #endif
 
   unsigned m_spiDivider;
