91baa4bb72623e6adc6d7868502c3c5e00891c48
Rich Holmes <rs.holmes@gmail.com>
2020-05-28 11:05:24 -0400

    Corrected output range for quantizer

--- software/examples/SimpleQuant/SimpleQuant.ino
+++ software/examples/SimpleQuant/SimpleQuant.ino
@@ -1,5 +1,6 @@
 /*
   Quantizing CV Input 1 to equal temperament
+  Use 0–10V CV output setting, we will send only 0 to 2047 so output is 0–5V matching input
  */
 
 #include <dac_ino.h>
@@ -24,10 +25,10 @@ int cvOutput = 0;
 void loop() {
   // Read CV Input 1
   cvInput = dac_inoBoard.readCV(dac_ino::CVInChannel::A);
-  // Add 2 bits
-  cvOutput = cvInput << 2;
+  // Add 1 bit (5V input is 1023, 5V output is 2048)
+  cvOutput = cvInput << 1;
   // Quantize
-  cvOutput = int(cvOutput / NOTE + 0.5) * NOTE;
+  cvOutput = constrain (int (int(cvOutput / NOTE + 0.5) * NOTE + 0.5), 0, 4095); // hardware requirement but should be 0 to 2048
   // Play quantized note on CV Output 1
   dac_inoBoard.writeCV(dac_ino::CVOutChannel::A, cvOutput);
   
